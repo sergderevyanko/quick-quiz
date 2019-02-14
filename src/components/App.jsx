654c3fb899a4ac6aa871d0e50e565d65a796379e
Sergey Derevyanko <sergey.derevyanko@opower.com>
2019-02-14 10:58:33 +0200

    Working quiz project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,77 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx';
+import Scorebox from './quiz/Scorebox.jsx';
+import Results from './quiz/Results.jsx';
 
 class App extends Component{
 
     constructor(props){
         super(props);
+        this.state = {
+          questions: [
+              {
+                  id: 1,
+                  text: 'What is your name?',
+                  choices: [
+                      {id: 'a', text: 'Michael'},
+                      {id: 'b', text: 'Brad'},
+                      {id: 'c', text: 'Steven'}
+                  ],
+                  correct: 'b'
+              },
+              {
+                  id: 2,
+                  text: 'What is your mother\'s name?',
+                  choices: [
+                      {id: 'a', text: 'Sara'},
+                      {id: 'b', text: 'Sue'},
+                      {id: 'c', text: 'Donna'}
+                  ],
+                  correct: 'b'
+              },
+              {
+                  id: 3,
+                  text: 'What is your father\'s name?',
+                  choices: [
+                      {id: 'a', text: 'Bobby'},
+                      {id: 'b', text: 'Rick'},
+                      {id: 'c', text: 'Nik'}
+                  ],
+                  correct: 'a'
+              }
+          ],
+            score: 0,
+            current: 1
+        };
+    }
+
+    setCurrent(current){
+        this.setState({current});
+
+    }
+
+    setScore(score){
+        this.setState({score});
     }
 
     render(){
+        if(this.state.current > this.state.questions.length){
+            var scorebox = '';
+            var results = <Results {...this.state}/>
+        }
+        else{
+            var scorebox =  <Scorebox {...this.state} />;
+            var results = '';
+        }
+
         return (
             <div>
-                <h3>React works!</h3>
+                {scorebox}
+                <QuestionList {...this.state} setCurrent={this.setCurrent.bind(this)}
+                                              setScore={this.setScore.bind(this)}/>
+                {results}
             </div>
         )
     }
